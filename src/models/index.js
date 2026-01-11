b4b51737b2e7a91c80f8ae47898f465a61138397
Reza Panahi <rezabumanpanahi@gmail.com>
2026-01-11 11:25:10 +0430

    index added to modles

---
+++ src/models/index.js
@@ -0,0 +1,26 @@
+const sequelize = require("../config/database");
+
+const User=require("./user");
+const Item=require("./item");
+const Offer=require("./offer");
+const Deal=require("./deal");
+
+User.hasMany(Item,{foreignKey:"ownerId"});
+Item.belongsTo(User,{foreignKey:"ownerId"});
+
+User.hasMany(Offer,{foreignKey:"userId"});
+Offer.belongsTo(User,{foreignKey:"userId"});
+
+Item.hasMany(Offer,{foreignKey:"itemId"});
+Offer.belongsTo(Item,{foreignKey:"itemId"});
+
+User.hasMany(Deal,{as:"buyer",foreignKey:"buyerId"});
+User.hasMany(Deal,{as:"owner",foreignKey:"ownerId"});
+
+Deal.belongsTo(User,{as:"buyer",foreignKey:"buyerId"});
+Deal.belongsTo(User,{as:"owner",foreignKey:"ownerId"});
+
+Item.hasOne(Deal,{foreignKey:"itemId"});
+Deal.belongsTo(Item,{foreignKey:"itemId"});
+
+module.exports = {User,Item,Offer,Deal};
